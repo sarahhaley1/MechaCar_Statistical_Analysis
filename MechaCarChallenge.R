9d339cb21d4dd25f0b14f9daa21403e9866f0d7d
sarahhaley1 <66536405+sarahhaley1@users.noreply.github.com>
2021-01-25 21:53:22 -0800

    Add files via upload

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,6 +1,7 @@
 
 library(dplyr)
 
+## Linear Regression for MechaCar data
 # loading the data set 
 MechaCar <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
 View(MechaCar)
@@ -11,3 +12,22 @@ linear_model <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + groun
 # finding the summary statistics to determine the p-value and r-squared value
 summ_stats <- summary(linear_model)
 summ_stats
+
+## Suspension Coil Technical Analysis 
+SuspensionCoil <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+View(SuspensionCoil)
+
+total_summary <- SuspensionCoil %>% select(PSI) %>%
+  summarize(meanPSI = mean(PSI), medianPSI = median(PSI), variancePSI = var(PSI), stdPSI = sd(PSI))
+
+total_summary <- as.data.frame(total_summary)
+total_summary    
+
+
+## lot summary - grouping the data by lot number 
+
+lot_summary <- SuspensionCoil %>% group_by(Manufacturing_Lot) %>%
+  summarize(mean_byLot=mean(PSI), median_byLot=median(PSI), variance_byLot=var(PSI), std_byLot=sd(PSI))
+lot_summary
+
+
